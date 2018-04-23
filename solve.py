bc36fa075a92ee9aed5499d12f085d6a3b720a63
Sijia Wang <sijiawang@Sijias-MacBook-Pro.local>
2018-04-22 20:02:16 -0500

    add cnf generator

--- solve.py
+++ solve.py
@@ -1,28 +1,38 @@
 from SATInstance import SATInstance
 from solver import recursive_solve
 from watchlist import setup_watchlist
+from testcase_generator import testcase_generator
 import time
 
-test_file = 'test/test.txt'
-# test_file = 'test/w44-008.txt'
-with open(test_file) as file:
-    instance = SATInstance.from_file(file)
-watch_list = setup_watchlist(instance)
-n = len(instance.variables)
+def main():
+    len_CNF = 30 # length of CNF
+    num_var = 5  # number of variables
+    num_testcase = 5 # number of test cases
+    testcase_generator(len_CNF,num_var,num_testcase)
+    for test_id in range(num_testcase):
+        test_file = 'test/testcase'+str(test_id)+'.txt'
+        # test_file = 'test/w44-008.txt'
+        with open(test_file) as file:
+            instance = SATInstance.from_file(file)
+        watch_list = setup_watchlist(instance)
+        n = len(instance.variables)
 
-upp_first = False
-assignments = [None] * n
-start_time = time.time()
-assignments = recursive_solve(instance, watch_list, assignments, 0, upp_first)
-end_time = time.time()
-for assignment in assignments:
-    print(instance.assignment_to_string(assignment))
-print(end_time - start_time)
+        upp_first = False
+        assignments = [None] * n
+        start_time = time.time()
+        assignments = recursive_solve(instance, watch_list, assignments, 0, upp_first)
+        end_time = time.time()
+        for assignment in assignments:
+            print(instance.assignment_to_string(assignment))
+        print(end_time - start_time)
 
-assignments = [None] * n
-start_time = time.time()
-assignments = recursive_solve(instance, watch_list, assignments, 0, not upp_first)
-end_time = time.time()
-for assignment in assignments:
-    print(instance.assignment_to_string(assignment))
-print(end_time - start_time)
+        assignments = [None] * n
+        start_time = time.time()
+        assignments = recursive_solve(instance, watch_list, assignments, 0, not upp_first)
+        end_time = time.time()
+        for assignment in assignments:
+            print(instance.assignment_to_string(assignment))
+        print(end_time - start_time)
+
+if __name__ == '__main__':
+    main()
---
+++ testcase_generator.py
@@ -0,0 +1,27 @@
+# This is a file of test cases for cse513 final project SAT-SOLVER
+
+import numpy as np
+# lens     : number of clauses
+# xnum     : number of variables
+# test_num : number of test cases
+
+
+def generate_CNF(lens, xnum):
+    rand = np.random.randint(-1, 2, size=(xnum, lens))
+    res = [[''] * lens for i in range(xnum)]
+    for i in range(xnum):
+        s = str(chr(65+i))
+        res[i] = list(np.where(rand[i] > 0,  s, ''))
+        res[i] = list(np.where(rand[i] < 0, '~'+s, res[i]))
+    res = np.transpose(res)
+    out = []
+    for i in range(lens):
+        out.append(str(' '.join(res[i])))
+    return out
+
+def testcase_generator(lens, xnum, test_num):
+    for i in range(test_num):
+        f= open("test/testcase"+str(i)+".txt","w+")
+        this_CNF = generate_CNF(lens,xnum)
+        for i in range(lens):
+            f.write(this_CNF[i]+"\n")
